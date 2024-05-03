11e30d2145bfebe06bd4477b2c820637e496aac8
odegay <dolegan@gmail.com>
2024-05-02 21:13:22 -0400

    fixing setup.py to include dependencies

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="adpipsvcfuncs",
-    version="0.1.4",
+    version="0.1.5",
     packages=find_packages(),
     install_requires=[],
     author="Oleg D",
@@ -10,5 +10,8 @@ setup(
     description="Service functions for the Adaptive Pipeline Workflow project.",
     license="MIT",
     keywords="GenAI, GCP",
-    url="https://github.com/odegay/adaptive-pipeline-service-functions",            
+    url="https://github.com/odegay/adaptive-pipeline-service-functions",
+    install_requires=[
+        'google-secret-manager', 'google-cloud-pubsub', 'requests'
+    ]
 )
